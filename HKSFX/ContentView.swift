d99dad69add8da64bb925d77ed706f4c37b11162
Scott Odle <scott@sjodle.com>
2022-03-19 16:16:45 -0700

    Initial Commit

---
+++ HKSFX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HKSFX
+//
+//  Created by Scott Odle on 3/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKSFX/HKSFXApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKSFXApp.swift
+//  HKSFX
+//
+//  Created by Scott Odle on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HKSFXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
